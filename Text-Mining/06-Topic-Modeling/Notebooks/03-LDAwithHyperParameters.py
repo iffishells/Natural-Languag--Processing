976e566327052b13b0782ff5d4dbb9ae09eba915
iffishells <iffishells@gmail.com>
2021-06-18 13:02:38 +0500

    Topic Modelling

---
+++ Text-Mining/06-Topic-Modeling/Notebooks/03-LDAwithHyperParameters.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 00:39:09 2019
+
+@author: Taimoor
+"""
+
+corpus = ['bread bread bread bread bread bread bread bread bread bread',
+         'milk milk milk milk milk milk milk milk milk milk',
+         'pet pet pet pet pet pet pet pet pet pet',
+         'bread bread bread bread bread bread bread bread bread bread milk milk milk milk milk milk milk milk milk milk']
+
+from sklearn.feature_extraction.text import CountVectorizer
+vec = CountVectorizer()
+matrix_X = vec.fit_transform(corpus)
+
+from sklearn.decomposition import LatentDirichletAllocation
+lda = LatentDirichletAllocation(n_components = 2, topic_word_prior = 0.1, doc_topic_prior = 0.1)
+lda.fit(matrix_X)
+
+for topic in lda.components_:
+    print([topic[t] for t in topic.argsort()[::-1]])
+    
+print(lda.transform(matrix_X))
---
+++ Text-Mining/06-Topic-Modeling/Notebooks/05-perplexity .py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 22:53:12 2019
+
+@author: Taimoor
+"""
+
+corpus = open('E:\\dataset2.csv').read()
+docs = corpus.split('\n')
+
+from sklearn.feature_extraction.text import CountVectorizer
+vec = CountVectorizer()
+matrix_X = vec.fit_transform(docs)
+
+from sklearn.decomposition import LatentDirichletAllocation
+lda1 = LatentDirichletAllocation(n_components = 3)
+lda2 = LatentDirichletAllocation(n_components = 2)
+
+lda1.fit(matrix_X[:500])
+lda2.fit(matrix_X[:500])
+
+print('lda1: ', lda1.perplexity(matrix_X[500:]))
+print('lda2: ', lda2.perplexity(matrix_X[500:]))
